868dfc5fcd34eec7271ef41ee347943b88c7566a
bradleyllew <51133387+bradleyllew@users.noreply.github.com>
2019-09-22 11:59:13 -0400

    Added keys into Spotify export

--- keys.js
+++ keys.js
@@ -1,6 +1,6 @@
 console.log('this is loaded');
 
 exports.spotify = {
-  id: process.env.SPOTIFY_ID,
-  secret: process.env.SPOTIFY_SECRET
+  id: process.env.d07600f779854425a221453f41b4fbd9,
+  secret: process.env.2f078f7ba6fd4276b8f3964fc99bcaae
 };
