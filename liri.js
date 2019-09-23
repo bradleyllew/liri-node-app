51fd06553e850ea335e86efe72091f135d0595d9
bradleyllew <51133387+bradleyllew@users.noreply.github.com>
2019-09-22 22:40:08 -0400

    Added and required Inquirer, then added start of prompts. Added .then function to take in user input and to console.log.  Added a fs.readFile, to read and implement from random.txt file if user doesn't choose a movie title. I feel like I have the right idea, maybe, but I'm missing something in execution.  Node is still throwing errors?

--- liri.js
+++ liri.js
@@ -2,19 +2,101 @@ require("dotenv").config();
 
 var fs = require("fs");
 
+var inquirer = require('inquirer');
+
 var axios = require("axios");
 
 var moment = require('moment');
 
 var keys = require("./keys.js");
-
 console.log(keys);
 
-var spotify = new Spotify(keys.spotify);
+var spotify = new spotify(keys.spotify);
 
-var omdb = new Omdb(keys.omdb);
+var omdb = new omdb(keys.omdb);
 
 var bands = new BIT(keys.bandsintown);
+
+
+// INQUIRER
+inquirer.prompt([
+
+    {
+        type: "input",
+        name: "name",
+        message: "Hi, what's your name?"
+    },
+
+    {
+        type: "list",
+        name: "wouldYouLikeTo",
+        message: "What would you like Liri to do?",
+        choices: ["concert-this", "spotify-this", "movie-this", "do-what-it-says"]
+    }
+
+]).then(function (user) {
+
+    // If the user chooses concert-this...
+    if (user.wouldYouLikeTo === "concert-this") {
+
+        console.log("==============================================");
+        console.log("");
+        
+        console.log("");
+        console.log("==============================================");
+        concertThis(concertThis);
+    }
+    // If user chooses spotify-this...
+    if (user.wouldYouLikeTo === "spotify-this") {
+
+        console.log("==============================================");
+        console.log("");
+        
+        console.log("");
+        console.log("==============================================");
+        spotifyThis(spotifyThis);
+    }
+    // If user chooses movie-this...
+    if (user.wouldYouLikeTo === "movie-this") {
+
+        console.log("==============================================");
+        console.log("");
+        
+        console.log("");
+        console.log("==============================================");
+        movieThis(movieThis);
+    } else {
+        // If user doesn't enter a movie name...
+        console.log("==============================================");
+        console.log("");
+        console.log("Sorry " + user.name);
+        console.log("If you haven't watched 'Mr. Nobody,' then you should!");
+        console.log("It's on Netflix!");
+        console.log("");
+        console.log("==============================================");
+        movieThis("http://www.imdb.com/title/tt0485947/");
+    }
+    // If user chooses would-you-like-to
+    if (user.wouldYouLikeTo === "do-what-it-says") {
+
+        fs.readFile("random.txt", "utf8", function (error, data) {
+            if (error) {
+                return console.log(error);
+            }
+            console.log(data);
+            var dataArr = data.split(",");
+            console.log(dataArr);
+        });
+        console.log("==============================================");
+        console.log("");
+        console.log("Well let's do it then, " + user.name);
+        console.log("");
+        console.log("==============================================");
+    }
+    spotifyThis(random.txt);
+});
+
+
 
 
 // OMDB:
@@ -73,7 +155,7 @@ function movieThis() {
         });
 };
 
-movieThis();
+movieThis(movieThis);
 
 // SPOTIFY
 function spotifyThis() {
@@ -127,7 +209,7 @@ function spotifyThis() {
         });
 };
 
-spotifyThis();
+spotifyThis(spotifyThis);
 
 
 // BANDS IN TOWN
@@ -181,31 +263,31 @@ function concertThis() {
         });
 };
 
-concertThis();
+concertThis(concertThis);
 
 
 // BONUS OUTPUTTING THE DATA BY APPENDING EACH COMMAND
 
-fs.readFile("log.txt", "utf8", function(err, data) {
-  // If there's an error reading the file, we log it and return immediately
-  if (err) {
-    return console.log(err);
-  }
-
-  // Inside of the readFile callback, we use the appendFile function
-  // The first parameter is the name of the text file to save to
-  // The second parameter is the data we want to write as a string
-  // The third parameter is the callback function to be called when appendFile is finished
-  // For more info, see the docs: https://nodejs.org/api/fs.html#fs_fs_appendfile_path_data_options_callback
-  fs.appendFile("log.txt", "command", function(err) {
-    // If there was an error, we log it and return immediately
+fs.readFile("log.txt", "utf8", function (err, data) {
+    // If there's an error reading the file, we log it and return immediately
     if (err) {
-      return console.log(err);
+        return console.log(err);
     }
 
-    // log that we saved the info successfully. we know that
-    // because no error was encountered, or we would have returned above
-    console.log("SAVED");
+    // Inside of the readFile callback, we use the appendFile function
+    // The first parameter is the name of the text file to save to
+    // The second parameter is the data we want to write as a string
+    // The third parameter is the callback function to be called when appendFile is finished
+    // For more info, see the docs: https://nodejs.org/api/fs.html#fs_fs_appendfile_path_data_options_callback
+    fs.appendFile("log.txt", "command", function (err) {
+        // If there was an error, we log it and return immediately
+        if (err) {
+            return console.log(err);
+        }
 
-  });
+        // log that we saved the info successfully. we know that
+        // because no error was encountered, or we would have returned above
+        console.log("SAVED");
+
+    });
 });
